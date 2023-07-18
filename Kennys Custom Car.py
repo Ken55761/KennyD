ec0bd1f343ffb591416101ae645ee9162bd132d2
Ken55761 <139511297+Ken55761@users.noreply.github.com>
2023-07-18 19:28:44 -0400

    Add files via upload

---
+++ Kennys Custom Car.py
@@ -0,0 +1,43 @@
+#Create Welcome Message
+
+print("=============")
+print()
+print("Welcome To KennyD Used Cars")
+print("Customized Killer Rides")
+print()
+print("=============")
+
+# Determine Make and Model Customer Wants
+print("1. What Type Of Car Would You Like?")
+print("Please Select One Of The Following Models")
+print(" a. Mercedes-Benz EQS ")
+print(" b. Infinity Q70")
+print(" c. Jaguar XF")
+print(" d. Audi Q8")
+print(" e. Cadillac CT6")
+
+first = input("Please Enter Car 'a'-'e': ")
+
+# Determine The Year Of The Car
+print("2. What Model Year Would You Prefer")
+print(" a. 2021")
+print(" b. 2022")
+print(" c. 2023")
+second = input("Please Select Year 'a' - 'c': ")
+
+# Determine If They Want A Sedan or 4 Door
+print("3. Would You Prefer The 4 Door instead of a Sedan")
+third = input("Please Enter 'Yes' or 'No': ")
+
+# Determine The Color
+fourth = input ("Type Your Color Choice '   ': ")
+
+#Determine Seat Material Choice
+print("5. Choose Between Leather Or Cloth Seats")
+print(" a. Leather")
+print(" b. Cloth")
+fifth = input("Please Select Material 'a' - 'b': ")
+
+#Determine Name On Customized Plate
+print("Tell Us The Name You Want On Your Plate")
+sixth = input()
